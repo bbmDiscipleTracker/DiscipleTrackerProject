20801f9c7bf55bbf5218c828f480efb224419a92
bbmDiscipleTracker <53278425+bbmDiscipleTracker@users.noreply.github.com>
2019-10-15 01:57:58 +0200

    Updated SignIn

--- Discipleship/Discipleship/SignIn/signIn.aspx.designer.cs
+++ Discipleship/Discipleship/SignIn/signIn.aspx.designer.cs
@@ -40,13 +40,13 @@ namespace Discipleship.SignIn {
         protected global::System.Web.UI.WebControls.Button btnSignIn;
         
         /// <summary>
-        /// lnkForgotPassword control.
+        /// LinkButton1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnkForgotPassword;
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
         
         /// <summary>
         /// lblMessage control.
